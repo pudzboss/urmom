3839f3ebb0821cace8e565f50e27812038fac609
pudzboss <39745657+pudzboss@users.noreply.github.com>
2021-04-15 10:12:43 -0600

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/pudzboss/urmom
+
+go 1.16
+
+require gorgonia.org/gorgonia v0.9.17
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"gorgonia.org/gorgonia"
+)
+
+func main() {
+	fmt.Println("ill give u rabies")
+}
